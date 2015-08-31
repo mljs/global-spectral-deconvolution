55c264a70620aa8af9add8aacc69e8eef2c095db
Luc Patiny <luc@patiny.com>
2015-08-31 21:38:26 +0200

    Add experimental spectrum

--- test/ubiquitin.js
+++ test/ubiquitin.js
@@ -2,11 +2,9 @@
 
 var fs = require('fs');
 var gsd = require("..");
+var parser = require('xy-parser');
 
-
-var spectrum=JSON.parse(fs.readFileSync('./test//C2.json', 'utf-8'));
-
-
+var spectrum=parser.parse(fs.readFileSync('./test//ubiquitin.txt', 'utf-8'), {arrayType: 'xxyy'});
 
 
 
@@ -17,7 +15,6 @@ describe('Global spectra deconvolution', function () {
 
 
         var result = gsd(spectrum[0],spectrum[1], 0.001, 0.1);
-console.log("---------------")
         console.log(result);
 
     });
