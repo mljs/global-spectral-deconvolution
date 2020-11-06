4c4c92a00295d4ee82d0a7c3032b8c7d32b352f3
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-11-06 04:32:29 -0500

    chore: improve coverage

--- src/__tests__/simulated.js
+++ src/__tests__/simulated.js
@@ -1,26 +1,30 @@
-import { gsd } from '..';
+import { gsd, optimizePeaks } from '..';
 
-const { SpectrumGenerator } = require('spectrum-generator');
+const { generateSpectrum } = require('spectrum-generator');
 
 describe('Global spectra deconvolution with simulated spectra', () => {
   // Test case obtained from Pag 443, Chap 8.
   it('Should provide the right result ...', () => {
-    const sg = new SpectrumGenerator({ start: 0, end: 100, pointsPerUnit: 10 });
+    const peaks = [
+      { x: 0.1, y: 0.2, width: 0.1 },
+      { x: -0.1, y: 0.2, width: 0.3 },
+    ];
 
-    sg.addPeak([20, 100], { width: 5 });
-    sg.addPeak([50, 50], { width: 5 });
-    sg.addPeak([70, 20], { width: 5 });
+    const data = generateSpectrum(peaks, { from: -1, to: 1, nbPoints: 101 });
 
-    let spectrum = sg.getSpectrum();
-
-    let result = gsd(spectrum, {
+    let peakList = gsd(data, {
       minMaxRatio: 0,
       realTopDetection: false,
       smoothY: false,
     });
 
-    expect(result[0]).toMatchObject({ x: 20, y: 100 });
-    expect(result[1]).toMatchObject({ x: 50, y: 50 });
-    expect(result[2]).toMatchObject({ x: 70, y: 20 });
+    let optPeaks = optimizePeaks(data, peakList, {});
+
+    expect(optPeaks[0].x).toBeCloseTo(-0.1, 2);
+    expect(optPeaks[0].y).toBeCloseTo(0.2, 2);
+    expect(optPeaks[0].width).toBeCloseTo(0.3, 2);
+    expect(optPeaks[1].x).toBeCloseTo(0.1, 2);
+    expect(optPeaks[1].y).toBeCloseTo(0.2, 2);
+    expect(optPeaks[1].width).toBeCloseTo(0.1, 2);
   });
 });
