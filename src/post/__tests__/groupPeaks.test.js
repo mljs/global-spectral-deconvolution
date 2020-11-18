6ef7024a1e34ce63c71fa0679946df74271d3c88
Luc Patiny <luc@patiny.com>
2020-11-18 15:17:42 +0100

    feat: improve groupPeaks

---
+++ src/post/__tests__/groupPeaks.test.js
@@ -0,0 +1,70 @@
+import { groupPeaks } from '../groupPeaks';
+
+describe('groupPeaks', () => {
+  it('default factor value', () => {
+    let result = groupPeaks([
+      { x: 5, y: 10, width: 5 },
+      { x: 10, y: 10, width: 5 },
+      { x: 30, y: 10, width: 5 },
+    ]);
+    expect(result).toEqual([
+      [
+        { x: 5, y: 10, width: 5, group: 0 },
+        { x: 10, y: 10, width: 5, group: 0 },
+      ],
+      [{ x: 30, y: 10, width: 5, group: 1 }],
+    ]);
+  });
+
+  it('factor = 0.1', () => {
+    let result = groupPeaks(
+      [
+        { x: 5, y: 10, width: 5 },
+        { x: 10, y: 10, width: 5 },
+        { x: 30, y: 10, width: 5 },
+      ],
+      0.1,
+    );
+    expect(result).toEqual([
+      [{ x: 5, y: 10, width: 5, group: 0 }],
+      [{ x: 10, y: 10, width: 5, group: 1 }],
+      [{ x: 30, y: 10, width: 5, group: 2 }],
+    ]);
+  });
+
+  it('factor=3', () => {
+    let result = groupPeaks(
+      [
+        { x: 5, y: 10, width: 5 },
+        { x: 10, y: 10, width: 5 },
+        { x: 30, y: 10, width: 5 },
+      ],
+      3,
+    );
+    expect(result).toEqual([
+      [
+        { x: 5, y: 10, width: 5, group: 0 },
+        { x: 10, y: 10, width: 5, group: 0 },
+      ],
+      [{ x: 30, y: 10, width: 5, group: 1 }],
+    ]);
+  });
+
+  it('factor=5', () => {
+    let result = groupPeaks(
+      [
+        { x: 5, y: 10, width: 5 },
+        { x: 10, y: 10, width: 5 },
+        { x: 30, y: 10, width: 5 },
+      ],
+      5,
+    );
+    expect(result).toEqual([
+      [
+        { x: 5, y: 10, width: 5, group: 0 },
+        { x: 10, y: 10, width: 5, group: 0 },
+        { x: 30, y: 10, width: 5, group: 0 },
+      ],
+    ]);
+  });
+});
--- src/post/groupPeaks.js
+++ src/post/groupPeaks.js
@@ -1,63 +1,30 @@
-export function groupPeaks(peakList, nL, joinPeaks) {
-  let group = [];
+/**
+ * Group peaks based on factor and add group property in peaks
+ * @param {array} peakList
+ * @param {number} factor
+ */
+
+export function groupPeaks(peakList, factor = 1) {
+  if (peakList.length === 0) return [];
+  let peaks = peakList.sort((a, b) => a.x - b.x);
+
+  let previousPeak = { x: Number.NEGATIVE_INFINITY, width: 1 };
+  let currentGroup = [previousPeak];
   let groups = [];
-  let limits = [peakList[0].x, nL * peakList[0].width];
-  let upperLimit, lowerLimit;
-  // Merge forward
-  for (let i = 0; i < peakList.length; i++) {
-    // If the 2 things overlaps
+
+  for (let peak of peaks) {
     if (
-      joinPeaks &&
-      Math.abs(peakList[i].x - limits[0]) < nL * peakList[i].width + limits[1]
+      (peak.x - previousPeak.x) / (peak.width + previousPeak.width) <=
+      factor / 2
     ) {
-      // Add the peak to the group
-      group.push(peakList[i]);
-      // Update the group limits
-      upperLimit = limits[0] + limits[1];
-      if (peakList[i].x + nL * peakList[i].width > upperLimit) {
-        upperLimit = peakList[i].x + nL * peakList[i].width;
-      }
-      lowerLimit = limits[0] - limits[1];
-      if (peakList[i].x - nL * peakList[i].width < lowerLimit) {
-        lowerLimit = peakList[i].x - nL * peakList[i].width;
-      }
-      limits = [
-        (upperLimit + lowerLimit) / 2,
-        Math.abs(upperLimit - lowerLimit) / 2,
-      ];
+      currentGroup.push(peak);
     } else {
-      groups.push({ limits: limits, group: group });
-      group = [peakList[i]];
-      limits = [peakList[i].x, nL * peakList[i].width];
+      currentGroup = [peak];
+      groups.push(currentGroup);
     }
+    peak.group = groups.length - 1;
+    previousPeak = peak;
   }
-  groups.push({ limits: limits, group: group });
-  // Merge backward
-  for (let i = groups.length - 2; i >= 0; i--) {
-    // The groups overlaps
-    if (
-      Math.abs(groups[i].limits[0] - groups[i + 1].limits[0]) <
-      (groups[i].limits[1] + groups[i + 1].limits[1]) / 2
-    ) {
-      for (let j = 0; j < groups[i + 1].group.length; j++) {
-        groups[i].group.push(groups[i + 1].group[j]);
-      }
-      upperLimit = groups[i].limits[0] + groups[i].limits[1];
-      if (groups[i + 1].limits[0] + groups[i + 1].limits[1] > upperLimit) {
-        upperLimit = groups[i + 1].limits[0] + groups[i + 1].limits[1];
-      }
-      lowerLimit = groups[i].limits[0] - groups[i].limits[1];
-      if (groups[i + 1].limits[0] - groups[i + 1].limits[1] < lowerLimit) {
-        lowerLimit = groups[i + 1].limits[0] - groups[i + 1].limits[1];
-      }
 
-      groups[i].limits = [
-        (upperLimit + lowerLimit) / 2,
-        Math.abs(upperLimit - lowerLimit) / 2,
-      ];
-
-      groups.splice(i + 1, 1);
-    }
-  }
   return groups;
 }
