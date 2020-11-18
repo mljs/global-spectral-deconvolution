49c61cf3e6eb7ff3751d51eee92131c88924186b
Luc Patiny <luc@patiny.com>
2020-11-18 13:25:59 +0100

    refactor: extract optimizePeaks  function

--- src/post/optimizePeaks.js
+++ src/post/optimizePeaks.js
@@ -1,5 +1,7 @@
 import { optimize } from 'ml-spectra-fitting';
 
+import { groupPeaks } from './groupPeaks';
+
 /**
  * Optimize the position (x), max intensity (y), full width at half maximum (width)
  * and the ratio of gaussian contribution (mu) if it's required. It supports three kind of shapes: gaussian, lorentzian and pseudovoigt
@@ -113,68 +115,4 @@ function sampleFunction(from, to, x, y, lastIndex) {
   }
   lastIndex[0] = index;
   return { x: sampleX, y: sampleY };
-}
-
-function groupPeaks(peakList, nL, joinPeaks) {
-  let group = [];
-  let groups = [];
-  let limits = [peakList[0].x, nL * peakList[0].width];
-  let upperLimit, lowerLimit;
-  // Merge forward
-  for (let i = 0; i < peakList.length; i++) {
-    // If the 2 things overlaps
-    if (
-      joinPeaks &&
-      Math.abs(peakList[i].x - limits[0]) < nL * peakList[i].width + limits[1]
-    ) {
-      // Add the peak to the group
-      group.push(peakList[i]);
-      // Update the group limits
-      upperLimit = limits[0] + limits[1];
-      if (peakList[i].x + nL * peakList[i].width > upperLimit) {
-        upperLimit = peakList[i].x + nL * peakList[i].width;
-      }
-      lowerLimit = limits[0] - limits[1];
-      if (peakList[i].x - nL * peakList[i].width < lowerLimit) {
-        lowerLimit = peakList[i].x - nL * peakList[i].width;
-      }
-      limits = [
-        (upperLimit + lowerLimit) / 2,
-        Math.abs(upperLimit - lowerLimit) / 2,
-      ];
-    } else {
-      groups.push({ limits: limits, group: group });
-      group = [peakList[i]];
-      limits = [peakList[i].x, nL * peakList[i].width];
-    }
-  }
-  groups.push({ limits: limits, group: group });
-  // Merge backward
-  for (let i = groups.length - 2; i >= 0; i--) {
-    // The groups overlaps
-    if (
-      Math.abs(groups[i].limits[0] - groups[i + 1].limits[0]) <
-      (groups[i].limits[1] + groups[i + 1].limits[1]) / 2
-    ) {
-      for (let j = 0; j < groups[i + 1].group.length; j++) {
-        groups[i].group.push(groups[i + 1].group[j]);
-      }
-      upperLimit = groups[i].limits[0] + groups[i].limits[1];
-      if (groups[i + 1].limits[0] + groups[i + 1].limits[1] > upperLimit) {
-        upperLimit = groups[i + 1].limits[0] + groups[i + 1].limits[1];
-      }
-      lowerLimit = groups[i].limits[0] - groups[i].limits[1];
-      if (groups[i + 1].limits[0] - groups[i + 1].limits[1] < lowerLimit) {
-        lowerLimit = groups[i + 1].limits[0] - groups[i + 1].limits[1];
-      }
-
-      groups[i].limits = [
-        (upperLimit + lowerLimit) / 2,
-        Math.abs(upperLimit - lowerLimit) / 2,
-      ];
-
-      groups.splice(i + 1, 1);
-    }
-  }
-  return groups;
 }
