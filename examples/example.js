480211654cf20843ee597fc4cea0d30b817daf67
Luc Patiny <luc@patiny.com>
2020-12-05 07:22:58 +0100

    chore: add advanced example

--- examples/example.js
+++ examples/example.js
@@ -1,14 +1,49 @@
-const gsd = require('../src/gsd');
+/*
+install vscode plugin `liveserver` and right click on index.html and `Open with liveserver`
 
-let X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-let Y = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0];
-let peaks = gsd(X, Y, {
-  noiseLevel: 0,
-  minMaxRatio: 0.00025, // Threshold to determine if a given peak should be considered as a noise
-  realTopDetection: true,
-  maxCriteria: true, // inverted:false
+ start this code with automatic reload using
+`npm run example``
+*/
+
+import { gsd, optimizePeaks } from '../src';
+import { writeFileSync } from 'fs'
+import { join } from 'path'
+import { generateSpectrum } from 'spectrum-generator'
+
+const peaks = [
+  { x: -0.1, y: 0.2, width: 0.3 },
+  { x: 0.1, y: 0.2, width: 0.1 },
+];
+
+const original = generateSpectrum(peaks, { from: -1, to: 1, nbPoints: 101 });
+
+
+let peakList = gsd(original, {
+  minMaxRatio: 0,
+  realTopDetection: false,
   smoothY: false,
-  sgOptions: { windowSize: 7, polynomial: 3 },
+  heightFactor: 1,
+  shape: { kind: 'gaussian' },
 });
 
-console.log(peaks);
+
+let optimizedPeaks = optimizePeaks(original, peakList);
+
+let labels = optimizedPeaks.map(peak => ({ x: peak.x, y: peak.y, dy: '-10px', dx: '3px', text: peak.x.toPrecision(4) + ' / ' + peak.y.toPrecision(4) }));
+let lines = optimizedPeaks.map(peak => ([{ x: peak.x, y: peak.y }, { x: peak.x, y: peak.y, dy: '-20px' }]));
+let polygons = [];
+for (const peak of optimizedPeaks) {
+  const peaksSpectrum = generateSpectrum([peak], { from: peak.x - peak.width * 5, to: peak.x + peak.width * 5, nbPoints: 501 });
+  const polygon = [];
+  for (let i = 0; i < peaksSpectrum.x.length; i++) {
+    polygon.push({ x: peaksSpectrum.x[i], y: peaksSpectrum.y[i] })
+  }
+  polygons.push(polygon);
+}
+
+
+
+original.x = Array.from(original.x)
+original.y = Array.from(original.y)
+
+writeFileSync(join(__dirname, 'data.json'), JSON.stringify({ data: original, labels, lines, polygons }), 'utf8')
