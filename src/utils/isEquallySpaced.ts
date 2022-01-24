9484d8d7a96fd7be83751d9d459f273e35a2b324
Luc Patiny <luc@patiny.com>
2022-01-24 15:53:36 +0100

    chore: rename variables in isEquallySpaced

--- src/utils/isEquallySpaced.ts
+++ src/utils/isEquallySpaced.ts
@@ -1,16 +1,20 @@
 import { DoubleArray } from 'cheminfo-types';
 
+/**
+ * We check that the maximal slot with is not larger that 5% of the minimal slot width
+ * @param x
+ * @returns
+ */
 export function isEquallySpaced(x: DoubleArray): boolean {
-  let tmp: number;
   let maxDx = 0;
   let minDx = Number.MAX_SAFE_INTEGER;
   for (let i = 0; i < x.length - 1; ++i) {
-    tmp = Math.abs(x[i + 1] - x[i]);
-    if (tmp < minDx) {
-      minDx = tmp;
+    let absoluteDifference = Math.abs(x[i + 1] - x[i]);
+    if (absoluteDifference < minDx) {
+      minDx = absoluteDifference;
     }
-    if (tmp > maxDx) {
-      maxDx = tmp;
+    if (absoluteDifference > maxDx) {
+      maxDx = absoluteDifference;
     }
   }
   return (maxDx - minDx) / maxDx < 0.05;
