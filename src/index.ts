2967ef5126e5bcec38ec37ec7712a2749aa1f398
Luc Patiny <luc@patiny.com>
2022-02-14 17:25:30 +0100

    fix: export typescript interface for GSDPeak, etc.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export * from './post/optimizePeaks';
 // joinBroadPeaks is not tested and should not be used
 // export * from './post/joinBroadPeaks';
 export * from './post/broadenPeaks';
+
+export * from './GSDBroadenPeak';
+export * from './GSDPeak';
+export * from './GSDPeakOptimized';
