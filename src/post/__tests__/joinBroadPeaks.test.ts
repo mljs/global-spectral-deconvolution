de31772ba13e4221140095083f5ecaf67a1949fc
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-02-16 11:24:20 +0100

    fix: refactor joinBroadPeaks

---
+++ src/post/__tests__/joinBroadPeaks.test.ts
@@ -0,0 +1,33 @@
+import { generateSpectrum } from 'spectrum-generator';
+
+import { gsd } from '../../gsd';
+import { joinBroadPeaks } from '../joinBroadPeaks';
+
+describe('joinBroadPeaks', () => {
+  it('mix broad and not broad peaks', () => {
+    const data = generateSpectrum(
+      [
+        { x: 0.25, y: 10, width: 0.08 },
+        { x: 0.4, y: 40, width: 0.005 },
+        { x: 0.39, y: 40, width: 0.005 },
+      ],
+      {
+        generator: {
+          from: 0,
+          to: 0.5,
+          nbPoints: 1024,
+        },
+        noise: { seed: 10, percent: 0.1 },
+      },
+    );
+
+    const peaks = gsd(data);
+    const newPeaks = joinBroadPeaks(data, peaks, {
+      broadRatio: 0.03,
+      broadWidth: 0.25,
+    });
+
+    expect(peaks.length).toBeGreaterThan(3);
+    expect(newPeaks).toHaveLength(3);
+  });
+});
