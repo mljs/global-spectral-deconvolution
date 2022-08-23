6799f90efd8eb30625d23db2656fb059eae031c2
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-08-23 11:22:08 -0500

    feat: addMissingIDs

---
+++ src/utils/addMissingIDs.ts
@@ -0,0 +1,17 @@
+import { v4 as generateID } from '@lukeed/uuid';
+
+const { parse, stringify } = JSON;
+
+export function addMissingIDs<T extends { id?: string }>(
+  peaks: T[],
+  options: { output?: T[] } = {},
+) {
+  const { output = parse(stringify(peaks)) as T[] } = options;
+  for (const peak of output) {
+    if (!('id' in peak)) {
+      peak.id = generateID();
+    }
+  }
+
+  return output as (T & { id: string })[];
+}
