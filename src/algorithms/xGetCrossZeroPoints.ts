40b1fd13e3b574208d6dbff54595df35b9af5158
jobo322 <jose.bolanos@correounivalle.edu.co>
2025-11-18 08:49:37 -0500

    fix: check than back and next index is less and greater than zero

--- src/algorithms/xGetCrossZeroPoints.ts
+++ src/algorithms/xGetCrossZeroPoints.ts
@@ -1,23 +1,26 @@
 import type { PeakData } from './PeakData.ts';
 
-export type xGetCrossZeroPointsInput = Pick<PeakData, 'y' | 'dY'>;
-export function xGetCrossZeroPoints(input: xGetCrossZeroPointsInput) {
+export type XGetCrossZeroPointsInput = Pick<PeakData, 'y' | 'dY'>;
+export function xGetCrossZeroPoints(input: XGetCrossZeroPointsInput) {
   const { y, dY } = input;
 
   const crossDy: number[] = [];
 
   for (let i = 1; i < y.length - 1; ++i) {
-    if ((dY[i] < 0 && dY[i + 1] > 0) || (dY[i] > 0 && dY[i + 1] < 0)) {
+    if (isLessAndGreaterThanZero(dY[i], dY[i + 1])) {
       // push the index of the element closer to zero
       crossDy.push(Math.abs(dY[i]) < Math.abs(dY[i + 1]) ? i : i + 1);
     } else if (
       // Handle exact zero
       dY[i] === 0 &&
-      dY[i] < Math.abs(dY[i + 1]) &&
-      dY[i] < Math.abs(dY[i - 1])
+      isLessAndGreaterThanZero(dY[i - 1], dY[i + 1])
     ) {
       crossDy.push(i);
     }
   }
   return crossDy;
+}
+
+function isLessAndGreaterThanZero(back: number, next: number) {
+  return (back < 0 && next > 0) || (back > 0 && next < 0);
 }
