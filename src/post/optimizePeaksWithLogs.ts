23e8a42c0ba33d03165a4a502d810ecd8bf1aa52
jobo322 <jose.bolanos@correounivalle.edu.co>
2026-09-02 18:27:59 -0500

    chore: uses for-of loop

--- src/post/optimizePeaksWithLogs.ts
+++ src/post/optimizePeaksWithLogs.ts
@@ -124,11 +124,11 @@ export function optimizePeaksWithLogs<T extends Peak>(
         optimization,
       });
 
-      for (let i = 0; i < optimizedPeaks.length; i++) {
+      for (const optimizedPeak of optimizedPeaks) {
         results.push({
-          ...optimizedPeaks[i],
-          width: getShape1D(optimizedPeaks[i].shape).fwhmToWidth(
-            optimizedPeaks[i].shape.fwhm,
+          ...optimizedPeak,
+          width: getShape1D(optimizedPeak.shape).fwhmToWidth(
+            optimizedPeak.shape.fwhm,
           ),
         } as GSDPeakOptimizedIDOrNot<T>);
       }
